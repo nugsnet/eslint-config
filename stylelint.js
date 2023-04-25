ce0f139ef7370b75d3e4356b3abdedf8c6a9ddc6
Yan Zhabin <ian.zhabin@gmail.com>
2023-04-25 15:07:18 -0700

    Update dependencies.

--- stylelint.js
+++ stylelint.js
@@ -2,5 +2,14 @@ module.exports = {
     extends: [
       "stylelint-config-standard-scss",
       "stylelint-config-standard-vue/scss"
-    ]
+    ],
+    rules: {
+      "selector-class-pattern": null,
+      "indentation": [
+        2,
+        {
+          "baseIndentLevel": 1
+        }
+      ]
+    }
   }
