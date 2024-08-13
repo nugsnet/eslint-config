4532cddc065c437dbbe4126765c166becc0599f5
Yan Zhabin <iyanello@gmail.com>
2024-08-12 21:48:05 -0700

    Add test file.

---
+++ vue/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.SomeComponent">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const foo = 'bar'
+
+  console.log(foo)  
+</script>
+
+<style lang="scss" module>
+  .SomeComponent {
+    // style of component
+    display: block;
+  }
+</style>
